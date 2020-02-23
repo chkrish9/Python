b7148ce4157f241ada70c1bf6d66ab31fef652c1
Murali <murali.24.4.91@gmail.com>
2020-02-23 00:11:09 -0600

    ICP5

--- ICP5/ICP5_1.py
+++ ICP5/ICP5_1.py
@@ -3,26 +3,30 @@ import matplotlib.pyplot as plt
 from scipy import stats
 import numpy as np
 
-train = pd.read_csv('train.csv')
-x = train['GarageArea']
-y = train['SalePrice']
+train = pd.read_csv('Data/train.csv')
+
+# Scatter Plot before removing outlier
 garage_area = train['GarageArea']
 sales_price = train['SalePrice']
 plt.scatter(garage_area, sales_price, alpha=.75, color='b')
-plt.xlabel('GarageArea')
-plt.ylabel('SalePrice')
+plt.xlabel('Garage Area')
+plt.ylabel('Sale Price')
 plt.title('Linear Regression Model')
 plt.show()
+
+# Removing outlier by using zscore
+# Z score is the relationship between MEAN and Standard Deviation.
 data = pd.concat([train['GarageArea'], train['SalePrice']], axis=1)
-print(x)
 z = np.abs(stats.zscore(data))
 threshold = 3
 data = data[(z < 3).all(axis=1)]
 print(data)
+
+# Scatter Plot after removing outlier
 garage_area = data['GarageArea']
 sales_price = data['SalePrice']
 plt.scatter(garage_area, sales_price, alpha=.75, color='b')
-plt.xlabel('GarageArea')
-plt.ylabel('SalePrice')
+plt.xlabel('Garage Area')
+plt.ylabel('Sale Price')
 plt.title('Linear Regression Model')
 plt.show()
--- ICP5/ICP5_2.py
+++ ICP5/ICP5_2.py
@@ -1,28 +1,34 @@
 import pandas as pd
 import numpy as np
 
-train = pd.read_csv('winequality-red.csv')
-
-##handling missing value
-data = train.select_dtypes(include=[np.number]).interpolate().dropna()
-##Build a linear model
-y = train.quality
-
-c = data.corr(method ='pearson')
-s = c.unstack()
-so = s.sort_values(kind="quicksort")
-
-print(so[0:3])
-
-X = data.drop(['quality'], axis=1)
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42, test_size=.33)
 from sklearn import linear_model
-lr = linear_model.LinearRegression()
-model = lr.fit(X_train, y_train)
-##Evaluate the performance and visualize results
-print ("R^2 is: \n", model.score(X_test, y_test))
-predictions = model.predict(X_test)
+from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
-print ('RMSE is: \n', mean_squared_error(y_test, predictions))
+
+train = pd.read_csv('Data/winequality-red.csv')
+
+# Handling missing value
+data = train.select_dtypes(include=[np.number]).interpolate().dropna()
+
+data_correlation = data.corr(method='pearson')
+unstack_data = data_correlation.unstack()
+sorted_data = unstack_data.sort_values(kind='quicksort')
+
+print(sorted_data[0:3])
+
+
+target_data = train.quality
+features_data = data.drop(['quality'], axis=1)
+
+features_train, features_test, target_train, target_test = train_test_split(features_data, target_data, random_state=42, test_size=.33)
+
+
+lr = linear_model.LinearRegression()
+model = lr.fit(features_train, target_train)
+
+# Evaluate the performance
+print("R^2 is: \n", model.score(features_test, target_test))
+predictions = model.predict(features_test)
+
+print('RMSE is: \n', mean_squared_error(target_test, predictions))
 
