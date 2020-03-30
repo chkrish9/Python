bf522ac36e8837966404a030327d220a675a64b4
Murali <murali.24.4.91@gmail.com>
2020-03-30 17:33:30 -0400

    changes

--- Deep_Learning/ICP1/ICP1_1.py
+++ Deep_Learning/ICP1/ICP1_1.py
@@ -1,18 +1,18 @@
 import pandas
 from keras.models import Sequential
 from keras.layers.core import Dense, Activation
-
-# load dataset
 from sklearn.model_selection import train_test_split
 import pandas as pd
 import numpy as np
 
+# load dataset
 dataset = pd.read_csv("diabetes.csv", header=None).values
 
 X_train, X_test, Y_train, Y_test = train_test_split(dataset[:,0:8], dataset[:,8],
                                                     test_size=0.25, random_state=87)
 np.random.seed(155)
-my_first_nn = Sequential() # create model
+# create model
+my_first_nn = Sequential()
 my_first_nn.add(Dense(20, input_dim=8, activation='relu')) # hidden layer
 my_first_nn.add(Dense(1, activation='sigmoid')) # output layer
 my_first_nn.compile(loss='binary_crossentropy', optimizer='adam', metrics=['acc'])
